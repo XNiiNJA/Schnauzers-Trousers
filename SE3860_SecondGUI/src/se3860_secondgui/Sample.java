4cd0802a1320a099a372f4230dffb84a6b6694f2
normanel <normanel@uwplatt.edu>
2016-03-17 17:10:21 -0500

    Added comments for sample class and samplehandler class. Edited some code for readability, no changes in functionality.

--- SE3860_SecondGUI/src/se3860_secondgui/Sample.java
+++ SE3860_SecondGUI/src/se3860_secondgui/Sample.java
@@ -1,7 +1,7 @@
 /**
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+   A Sample is a list of characters pertaining to a corresponding list of
+   years. Each year has a corresponding character to represent what info was
+   taken.
 */
 
 package se3860_secondgui;
@@ -15,38 +15,54 @@ public class Sample
    private String id;
    private char data[];
    
-   
-   
+   /*
+      Constructor for a Sample, the info[] represents the list of characters
+      representing the info that was recorded. idName is the name of the
+      given Sample.
+   */
    Sample(char [] info, String idName )
    {
       data = new char[info.length];
-      for(int i = 0; i < info.length; i ++)
-      {
-         data[i] = info[i];
-      }
+      System.arraycopy(info, 0, data, 0, info.length);
       id = idName;
    }
    
+   /*
+      Returns the id for a given Sample.
+   */
    public String GetId()
    {
       return id;
    }
    
+   /*
+      Sets the id of a cetain Sample.
+   */
    public void EditId( String newId)
    {
       id = newId;
    } 
    
+   /*
+      Returns the char[] for a certain Sample.
+   */
    public char [] getInfo()
    {
       return data;
    }
    
+   /*
+      Returns the length of the char[] for a certain Sample.
+      This is the same as the number of years records were taken.
+   */
    public int getSize()
    {
        return data.length;
    }
    
+   /*
+      Overriden toString() method to return a formatted id.
+   */
    @Override
    public String toString()
    {
--- SE3860_SecondGUI/src/se3860_secondgui/SampleHandler.java
+++ SE3860_SecondGUI/src/se3860_secondgui/SampleHandler.java
@@ -1,7 +1,5 @@
 /**
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+   The SampleHandler is used to do operations on all of the Samples.
 */
 
 package se3860_secondgui;
@@ -19,12 +17,17 @@ public class SampleHandler
    private final int idLength;
    private final int numSamples;
    
-   SampleHandler( int numOfSamples, int startYear, int lastYear, int idLen, 
-         boolean newSet, char [][] infoSet)
+   /*
+      Constructor for a SampleHandler. Takes in the number of Samples, the
+      start year and end year of the recording, the length of the Sample id's,
+      a boolean to see if it is a new set or not, and the char[][] which holds
+      all of the recorded information (data for each Sample for each year).
+   */
+   SampleHandler( int numOfSamples, int startYear, int lastYear, int idLen, boolean newSet, char [][] infoSet)
    {
       idLength = idLen;
       numSamples = numOfSamples;
-      sampleList = new ArrayList<Sample>();//new Sample[numOfSamples];
+      sampleList = new ArrayList<>();
       char [] info;
       if(newSet)
       {
@@ -32,7 +35,6 @@ public class SampleHandler
          for( int i = 0; i < numOfSamples; i++)
          {
             Sample s = new Sample(info, Integer.toString(i + 1));
-            //sampleList[i] = s;
             sampleList.add(s);
          }
       }
@@ -41,12 +43,15 @@ public class SampleHandler
          for( int i = 0; i < numOfSamples; i++)
          {
             Sample s = new Sample(infoSet[i], Integer.toString(i + 1));
-            //sampleList[i] = s;
             sampleList.add(s);
          }
       }
    }
    
+   /*
+      Given a Sample id, checks the sampleList indices for the id. Returns
+      that specific index or return -1 if not found.
+   */
    private int Find( String idName )
    {
       for( int i = 0; i < numSamples; i++)
@@ -57,23 +62,34 @@ public class SampleHandler
       return -1;
    }
    
+   /*
+      Given a specific id to find, change that id to a new name. Check if the
+      new id is found in the sampleList, if not found then checks if an old
+      id was passed, if one was then find the index of the oldId and edit it
+      with the new id.
+      return 1 if able to edit
+      return 0 if no oldID was passed
+      return -1 if newID was already in use
+   */
    public int changeId(String newID, String oldID)
    {
-        if(Find(newID) == -1)
+        if(Find(newID) == -1) 
         {
-            if(oldID != null)
+            if(oldID != null) 
             {
                 int index = Find(oldID);
-                //sampleList[index].EditId(newID);
                 sampleList.get(index).EditId(newID);
                 return 1;
             }
-            return 0; 
-            
+            return 0;         
         }
         return -1;
    }
    
+   /*
+      Changes Sample names from the default (0-SampleListSize) to their
+      actual names.
+   */
    public void changeSampleNames(char[][] sampleIDNames)
    {
       char[] sampleID = new char[idLength];
@@ -83,42 +99,58 @@ public class SampleHandler
          {
             sampleID[j] = sampleIDNames[j][i];
          }
-         //sampleList[i].EditId(new String(sampleID));
          sampleList.get(i).EditId(new String(sampleID));
       }
    }
    
+   /*
+      Returns a String[] with all of the current names in sampleList.
+   */
    public String [] refreshIdnames()
    {
-       String [] names =  new String[numSamples];
-           for( int i = 0; i < numSamples; i++)
+      String [] names =  new String[numSamples];
+      for( int i = 0; i < numSamples; i++)
       {
-         //names[i] = sampleList[i].GetId();
-          names[i] = sampleList.get(i).GetId();
+         names[i] = sampleList.get(i).GetId();
       }
       return names;   
    }
    
+   /*
+      Checks if the given id is in the sampleList, if so then returns
+      the char[] for the Sample at the index of the given id.
+   */
    public char[] getSampleInfo(String Id)
    {
       int index = Find(Id);
       if(index != -1)
-         //return sampleList[index].getInfo();
-          return sampleList.get(index).getInfo();
+      {
+         return sampleList.get(index).getInfo();
+      }
       return null;
    }
 
+   /*
+      Returns the sample at a given index of sampleList.
+   */
    public Sample getSample(int i)
    {
        return sampleList.get(i);
    }
    
+   /*
+      Iterates through the sampleList and checks each index's Sample to see
+      if the Sample's id is equal to the given id.
+   */
    public Sample getSampleById(String id)
    {
         for(int i = 0; i < sampleList.size(); i++)
+        {
             if(sampleList.get(i).GetId().equals(id))
+            {
                 return sampleList.get(i);
-       
+            }
+        }
         return null;
    }
 
